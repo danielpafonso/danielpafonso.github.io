51c5456ca7cb2e3d6838804f2a0a0ed882bff3f3
Daniel Afonso <daniel.afonso@parceiros.nos.pt>
2023-09-07 09:06:21 +0100

    Test github action without commit

--- generate_static.py
+++ generate_static.py
@@ -1,5 +1,7 @@
 """ Generate static webpages """
-import os.path as path
+import os
+import shutil
+
 from datetime import datetime
 
 def read_qoutes_file(filename):
@@ -8,38 +10,50 @@ def read_qoutes_file(filename):
     with open(filename, "r", encoding="utf8") as ifp:
         raw = ifp.read().strip().split("\n")
     # process data
-    return [line.split("|") for line in raw]
+    qoutes = [line.split("|") for line in raw]
+    qoutes.sort(key=lambda x: x[0])
+    return qoutes
+
+def prep_static_folder(folder, imgs_folder="img"):
+    """ create static folder and copy the images to it """
+    # create folder
+    print("Creating static folder")
+    os.makedirs(folder, exist_ok=True)
+    # copy images
+    print("Copying images to static folder")
+    shutil.copytree(imgs_folder, os.path.join(folder, imgs_folder))
 
 def generate_index(data:list, input_folder, output_folder, filename="index.html"):
     """ Generate static page for today qoute """
     print("Generating index page")
     # read template
-    template_file = path.join(input_folder, f"template_{filename}")
+    template_file = os.path.join(input_folder, f"template_{filename}")
     with open(template_file, "r", encoding="utf8") as ifp:
         template = ifp.read()
-    
+
+    # set defaut qoute as the last one
+    phrase = data[-1][1]
+    author = data[-1][2]
     # get current qoute
     current_day = datetime.utcnow().strftime("%Y-%m-%d")
-    phrase = None
-    author = None
     for x in data:
         if x[0] == current_day:
             phrase = x[1]
             author = x[2]
             break
+    
     # replace values
-    # template = template.replace("{{Phrase}}", phrase).replace("{{Author}}", author)
     template = template.format(phrase=phrase, author=author)
 
     # write static
-    static_file = path.join(output_folder, filename)
+    static_file = os.path.join(output_folder, filename)
     with open(static_file, "w", encoding="utf8") as ofp:
         ofp.write(template)
 
 def generate_list(data, input_folder, output_folder, filename="list.html"):
     """ Generate static page for list of all qoutes """
     print("Generating list page")
-    template_file = path.join(input_folder, f"template_{filename}")
+    template_file = os.path.join(input_folder, f"template_{filename}")
     with open(template_file, "r", encoding="utf8") as ifp:
         template = ifp.read().split("\n")
 
@@ -61,7 +75,7 @@ def generate_list(data, input_folder, output_folder, filename="list.html"):
     template[list_idx] = "\n".join(rows)
 
     # write static
-    static_file = path.join(output_folder, filename)
+    static_file = os.path.join(output_folder, filename)
     with open(static_file, "w", encoding="utf8") as ofp:
         ofp.write("\n".join(template))
 
@@ -71,12 +85,18 @@ def generate_list(data, input_folder, output_folder, filename="list.html"):
 if __name__ == "__main__":
     filepath = "quotes.dsv"
     template_folder = "templates"
-    static_folder = "."
+    static_folder = "static"
     
     # read qoute file
     quotes = read_qoutes_file(filepath)
 
+    # prep output folder
+    prep_static_folder(static_folder)
+
     # generate static page from templates
     generate_index(quotes, template_folder, static_folder)
     generate_list(quotes, template_folder, static_folder)
+    ## dummy duplicate html
+    filename = "duplicates.html"
+    shutil.copyfile(filename, os.path.join(static_folder, filename))
     # generate_duplicates(quotes, template_folder, static_folder)
